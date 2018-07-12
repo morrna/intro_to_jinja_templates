97824832d77e568afb2f1a143de8bf569607ba9c
Nathan Morrison <yourstruly@nathanmorrison.net>
2018-07-11 19:04:02 -0600

    working html template &c

--- load.py
+++ load.py
@@ -7,15 +7,30 @@ def verify_directory(dir_name):
         os.makedirs(dir_name)
 
 if __name__ in ['__main__', '__console__']:
-    f = open('presentation.yaml', 'r')
-    beamer_dir = 'beamer_output'
-    verify_directory(beamer_dir)
-    slides = yaml.safe_load_all(f) # this produces a generator
+    with open('presentation.yaml', 'r') as f:
+        beamer_dir = 'beamer_output'
+        verify_directory(beamer_dir)
+        slides = yaml.safe_load_all(f) # this produces a generator
 
-    latex_env = Environment(  autoescape=False
-                            , loader=FileSystemLoader('templates')
-                           )
-    latex_env.globals['slides'] = slides
-    template = latex_env.get_template('beamer.tex')
-    with open(os.path.join(beamer_dir, 'presentation.tex'), 'w') as pf:
-        pf.write(template.render())
+        latex_env = Environment(  autoescape=False
+                                , loader=FileSystemLoader('templates')
+                               )
+        latex_env.globals['slides'] = slides
+        latex_env.globals['type'] = type
+        template = latex_env.get_template('beamer.tex')
+        with open(os.path.join(beamer_dir, 'presentation.tex'), 'w') as pf:
+            pf.write(template.render())
+
+    with open('presentation.yaml', 'r') as f:
+        html_dir = 'html_output'
+        verify_directory(html_dir)
+        slides = yaml.safe_load_all(f) # this produces a generator
+
+        html_env = Environment(  autoescape=True
+                               , loader=FileSystemLoader('templates')
+                              )
+        html_env.globals['slides'] = slides
+        html_env.globals['type'] = type
+        template = html_env.get_template('template.html')
+        with open(os.path.join(html_dir, 'presentation.html'), 'w') as pf:
+            pf.write(template.render())
